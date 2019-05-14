6d2655bf297fdf52568647cbba31cc55133aef27
Joohyung Kim <crybaby26@yonsei.ac.kr>
2019-05-15 07:02:27 +0900

    classifier added

--- classifier_test.py
+++ classifier_test.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     binary = utils.get_binary_representation(False)
     multi = utils.get_multiclass_representation(False)
     
-    testX = utils.vec_concatenate( next(multi), next(binary))
-    utils.train_randomforest( testX, next(multi))
+    utils.train_randomforest( 
+        utils.vec_concatenate( next(multi), next(binary)), next(multi))
--- classifier_train.py
+++ classifier_train.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     binary = utils.get_binary_representation(True)
     multi = utils.get_multiclass_representation(True)
     
-    trainX = utils.vec_concatenate( next(multi), next(binary))
-    utils.train_randomforest( trainX, next(multi))
+    utils.train_randomforest( 
+        utils.vec_concatenate( next(multi), next(binary)), next(multi))
