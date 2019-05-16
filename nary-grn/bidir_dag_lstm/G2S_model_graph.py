021cdad27422de793b609148159b0db18f2de35e
Joohyung Kim <crybaby26@yonsei.ac.kr>
2019-05-17 03:43:21 +0900

    adjusting parameters for debugging

--- nary-grn/bidir_dag_lstm/G2S_model_graph.py
+++ nary-grn/bidir_dag_lstm/G2S_model_graph.py
@@ -115,21 +115,21 @@ class ModelGraph(object):
                 [options.entity_num*self.encoder_dim], dtype=tf.float32)
 
         w2 = tf.get_variable("w2",
-                [options.entity_num*self.encoder_dim, options.class_num], dtype=tf.float32)
+                [options.entity_num*self.encoder_dim, options.entity_num*self.encoder_dim], dtype=tf.float32)
         b2 = tf.get_variable("b2",
-                [options.class_num], dtype=tf.float32)
+                [options.entity_num*self.encoder_dim], dtype=tf.float32)
         
-        # w3 = tf.get_variable("w3",
-        #         [10*self.encoder_dim, options.class_num], dtype=tf.float32)
-        # b3 = tf.get_variable("b3",
-        #         [options.class_num], dtype=tf.float32)
+        w3 = tf.get_variable("w3",
+                 [options.entity_num*self.encoder_dim, options.class_num], dtype=tf.float32)
+        b3 = tf.get_variable("b3",
+                 [options.class_num], dtype=tf.float32)
 
         # hidden layer 1
         hidden_out1 = tf.nn.relu(tf.matmul(entity_states, w1)+b1)
-        # hidden_out2 = tf.nn.relu(tf.matmul(hidden_out1, w2)+b2)
+        hidden_out2 = tf.nn.relu(tf.matmul(hidden_out1, w2)+b2)
 
         # [batch, class_num]
-        prediction =  tf.nn.softmax(tf.matmul(hidden_out1, w2) + b2)
+        prediction =  tf.nn.softmax(tf.matmul(hidden_out2, w3) + b3)
         prediction = _clip_and_normalize(prediction, 1.0e-6)
         self.output = tf.argmax(prediction, axis=-1, output_type=tf.int32)
 
